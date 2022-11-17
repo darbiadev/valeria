d5865877a388713a4bd1436090b85d0644af9aaf
shenanigansd <shenanigansd@users.noreply.github.com>
2022-11-17 23:15:00 +0000

    deploy: 6f830ed45a8665bd21462afe165fdd814c432619

--- implementors/poem_openapi/base/trait.OpenApi.js
+++ implementors/poem_openapi/base/trait.OpenApi.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["valeria"] = [{"text":"impl OpenApi for <a class=\"struct\" href=\"valeria/struct.Api.html\" title=\"struct valeria::Api\">Api</a>","synthetic":false,"types":["valeria::Api"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"valeria":[["impl OpenApi for <a class=\"struct\" href=\"valeria/struct.Api.html\" title=\"struct valeria::Api\">Api</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
